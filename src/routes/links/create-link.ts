e9866c306db74a728945d77a8ee4bb9282a82ece
israelcruzz <israelcruz.contato@gmail.com>
2024-07-14 19:23:18 -0300

    feat: add create link route

--- src/routes/links/create-link.ts
+++ src/routes/links/create-link.ts
@@ -1,3 +1,49 @@
 import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { prisma } from "../../lib/prisma";
 
-export async function createLink(app: FastifyInstance) {}
+export async function createLink(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trip/:tripId/links",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string(),
+        }),
+        body: z.object({
+          title: z.string(),
+          url: z.string(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { tripId } = request.params;
+      const { title, url } = request.body;
+
+      const findTripWithSameId = await prisma.trip.findUnique({
+        where: {
+          id: tripId,
+        },
+      });
+
+      if (!findTripWithSameId) {
+        throw new Error("Trip Not Found");
+      }
+
+      if (!findTripWithSameId.is_confirmed) {
+        throw new Error("Trip Not Found");
+      }
+
+      const link = await prisma.link.create({
+        data: {
+          title,
+          url,
+          tripId,
+        },
+      });
+
+      return { link: link.id };
+    }
+  );
+}
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod";
 import { createTrip } from "./routes/trips/create-trip";
+import { createLink } from "./routes/links/create-link";
 
 const app = fastify();
 
@@ -16,5 +17,6 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
+app.register(createLink);
 
 app.listen({ port: 3333 }).then(() => console.log("🚀 Server Running"));
